440e1f396b951d5a37c4f30568ee5a6d84a69156
Yentl <yentl@globee.com>
2019-10-03 14:07:50 +0200

    Added user table seeder class with default admin details. Called the user seeder class into main seeder class.

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'name' => 'Cafe Admin',
+            'email' => 'admin@cafe.joy',
+            'password' => Hash::make('test123')
+        ]);
+    }
+}
