3b0739d693cfd716efcfbd85524f9f89fce1e76f
Yentl <yentl@globee.com>
2019-10-03 18:40:17 +0200

    Updated bid factory to add bids to current seeded products.

--- database/factories/BidFactory.php
+++ database/factories/BidFactory.php
@@ -8,9 +8,10 @@ use App\Models\User;
 use Faker\Generator as Faker;
 
 $factory->define(Bid::class, function (Faker $faker) {
+    $product = Product::all()->random();
     return [
         'user_id' => factory(User::class)->create()->id,
-        'product_id' => factory(Product::class)->create()->id,
-        'amount' => $faker->randomFloat()
+        'product_id' => $product->id,
+        'amount' => $faker->randomFloat(2, 1,200)
     ];
 });
