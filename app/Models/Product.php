a6288fb38e32c32ebbb9c9ff81e3c02f25b9a2d3
Yentl <yentl@globee.com>
2019-10-03 15:48:08 +0200

    Added products migration. Added product model with properties.

---
+++ app/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $description
+ * @property string $image
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
+class Product extends Model
+{
+    //
+}
