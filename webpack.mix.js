dd61be7598566bb94ced53b4449eae044416020b
Yentl <yentl@globee.com>
2019-10-03 15:43:53 +0200

    Set the public file in mix file. Added mix for copying images from resources to public.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.setPublicPath('public');
+
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.copy('resources/images/', 'public/images/');
+
+mix.version();
