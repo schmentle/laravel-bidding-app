c85e5b4307208533be557ad39eade60662f65c4d
Yentl <yentl@globee.com>
2019-10-03 14:20:29 +0200

    Removed unused import in user model. Added user model properties.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
 class User extends Authenticatable
 {
     use Notifiable;
