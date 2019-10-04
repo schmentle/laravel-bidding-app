33aeb5bbe30324dd093d6f565bfcd270e6adbfce
Yentl <yentl@globee.com>
2019-10-04 18:03:22 +0200

    Added sku, price, visits properties to the product model. Added relationship for bids to product model. Added highest method to return highest bid on product model. Added average method to return average bid on product model.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,11 +10,42 @@ use Illuminate\Database\Eloquent\Model;
  * @property int $id
  * @property string $title
  * @property string $description
+ * @property string $sku
+ * @property float $price
  * @property string $image
+ * @property int $visits
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  */
 class Product extends Model
 {
-    //
+    /**
+     * Get bids for product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function bids()
+    {
+        return $this->hasMany(Bid::class);
+    }
+
+    /**
+     * Get highest bid for product
+     *
+     * @return mixed
+     */
+    public function highest()
+    {
+        return $this->bids()->orderBy('amount', 'desc')->first()->amount ?? '-';
+    }
+
+    /**
+     * Get average bid for product
+     *
+     * @return mixed
+     */
+    public function average()
+    {
+        return $this->bids()->get()->avg('amount') ?? '-';
+    }
 }
